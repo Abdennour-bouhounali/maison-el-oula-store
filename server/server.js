6d1ed570d7833a6f7b0abbc6f62ecd31152ccd4c
Abdennour-bouhounali <abdennour.bouhounali@g.enp.edu.dz>
2026-05-15 17:21:04 +0100

    fix: express 5 wildcard syntax and specify node engine 20.x

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use(cors({
 }));
 
 // ✅ Handle preflight requests for all routes
-app.options('*', cors());
+app.options('/*', cors());
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
